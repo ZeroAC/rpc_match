90c35c5c16154669393dd8a48360d1febb42c184
zeroacUbuntu22.04 <zeroac_study@163.com>
2022-07-18 11:08:15 +0800

    match process v2.0 expand the threshold over time

--- handler.go
+++ handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/ZeroAC/rpc_match/kitex_gen/match_service"
 	"github.com/ZeroAC/rpc_match/src/match_core"
@@ -17,26 +18,34 @@ var matchPool = match_core.NewMatchPool()
 //消息队列
 var messageQueue = match_core.NewMessageQueue()
 
+//消费者
 func ConsumeTask() {
-	for ch := range messageQueue.CH {
-		fmt.Printf("ch: %v\n", ch)
-		messageQueue.Mu.Lock()
-		a := messageQueue.Q.Front()
-		messageQueue.Q.Dequeue()
-		messageQueue.Mu.Unlock()
-		if a.FuncName == "add" {
-			matchPool.AddUser(a.User)
-		} else if a.FuncName == "remove" {
-			matchPool.RemoveUser(a.User.Id)
+	for {
+		select {
+		case <-messageQueue.CH:
+			{ //消息队列有新数据，则处理后完成匹配
+				messageQueue.Mu.Lock()
+				a := messageQueue.Q.Front()
+				messageQueue.Q.Dequeue()
+				messageQueue.Mu.Unlock()
+				if a.FuncName == "add" {
+					matchPool.AddUser(a.User)
+				} else if a.FuncName == "remove" {
+					matchPool.RemoveUser(a.User.Id)
+				}
+				matchPool.MatchProcess()
+			}
+		case <-time.After(time.Second):
+			{ //超时 则匹配重试
+				matchPool.MatchProcess()
+			}
 		}
-		matchPool.MatchProcess()
 	}
 }
 
 //以下两个为生产者
 // AddUser implements the MatchImpl interface.
 func (s *MatchImpl) AddUser(ctx context.Context, user *match_service.User, info string) (resp int32, err error) {
-	fmt.Printf("add user %v\n", user)
 	messageQueue.Mu.Lock()
 	defer messageQueue.Mu.Unlock()
 	messageQueue.AddUser(user)
--- src/match_core/match_pool.go
+++ src/match_core/match_pool.go
@@ -2,24 +2,27 @@ package match_core
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/ZeroAC/rpc_match/kitex_gen/match_service"
 )
 
 //匹配池 保存发出匹配请求的所有用户
+type PoolUser struct {
+	User      *match_service.User
+	waitTimes int32
+}
 type MatchPool struct {
-	Users []*match_service.User
+	Users []*PoolUser
 }
 
 func NewMatchPool() *MatchPool {
 	ans := MatchPool{}
-	ans.Users = make([]*match_service.User, 0)
+	ans.Users = make([]*PoolUser, 0)
 	return &ans
 }
 func (f *MatchPool) AddUser(user *match_service.User) {
-	f.Users = append(f.Users, user)
-
+	f.Users = append(f.Users, &PoolUser{user, 0})
+	fmt.Printf("add user %v\n", user)
 }
 func (f *MatchPool) RemoveByUserIndex(i int32) error {
 	f.Users = append(f.Users[:i], f.Users[i+1:]...)
@@ -27,8 +30,9 @@ func (f *MatchPool) RemoveByUserIndex(i int32) error {
 }
 func (f *MatchPool) RemoveUser(id int32) error {
 	for i, v := range f.Users {
-		if v.Id == id {
+		if v.User.Id == id {
 			f.RemoveByUserIndex(int32(i))
+			fmt.Printf("remove user %v\n", id)
 			return nil
 		}
 	}
@@ -43,23 +47,38 @@ func Abs(x int32) int32 {
 	}
 	return x
 }
+func Min(a, b int32) int32 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+//核心用户匹配算法
 func (f *MatchPool) MatchProcess() {
-	//更改匹配策略v1.0 分数差在100以内的进行匹配
+	//更改匹配策略v2.0 每个用户随着匹配轮数的增加 能接受的分数阈值也变大
 	//排序后必定是相邻的匹配
-	sort.Slice(f.Users, func(i, j int) bool {
-		return f.Users[i].Score < f.Users[j].Score
-	})
 	delIndex := []int{} //匹配成功的下标 待删除
-	for i := 1; i < len(f.Users); i++ {
-		a, b := f.Users[i-1], f.Users[i]
-		if Abs(a.Score-b.Score) <= 100 {
-			fmt.Printf("%v %v matched!\n", a, b)
-			delIndex = append(delIndex, i-1, i)
-			i++
+	mp := make(map[int]struct{})
+	initScope := 100 //初始为100
+	addScope := 50   //每轮一次加30
+	for i := 0; i < len(f.Users); i++ {
+		for j := i + 1; j < len(f.Users); j++ {
+			if _, ok := mp[j]; ok { //该下标若已删除 则跳过
+				continue
+			}
+			a, b := f.Users[i], f.Users[j]
+			//若a和b都能相互接受 即二者的差值小于等于二者阈值的最小值
+			if Abs(a.User.Score-b.User.Score) <= Min(a.waitTimes, b.waitTimes)*int32(addScope)+int32(initScope) {
+				fmt.Printf("%v %v matched!!\n", a, b)
+				delIndex = append(delIndex, i, j)
+				mp[j] = struct{}{}
+				break
+			}
 		}
 	}
 
-	newUser := make([]*match_service.User, 0)
+	newUser := make([]*PoolUser, 0)
 	for i, j := 0, 0; i < len(f.Users); i++ {
 		if j == len(delIndex) || i != delIndex[j] {
 			newUser = append(newUser, f.Users[i])
@@ -68,4 +87,12 @@ func (f *MatchPool) MatchProcess() {
 		}
 	}
 	f.Users = newUser //剔除掉匹配成功的人
+	f.AddWaitTimes()  //为匹配成功的人 等待次数+1
+}
+
+//讲匹配池内的所有用户的等待次数加1
+func (f *MatchPool) AddWaitTimes() {
+	for i := 0; i < len(f.Users); i++ {
+		f.Users[i].waitTimes++
+	}
 }
